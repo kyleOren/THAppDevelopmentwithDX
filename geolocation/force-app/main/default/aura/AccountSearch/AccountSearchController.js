3b2cf5a15b975f8e77375745cc5474c99031dbf2
Kyle Gibson <kyle.gibson@smarterbusiness.co.uk>
2019-07-08 11:30:42 +0200

    updated project with requirements from trailhead - build the geolocation app using salesforce cli

--- geolocation/force-app/main/default/aura/AccountSearch/AccountSearchController.js
+++ geolocation/force-app/main/default/aura/AccountSearch/AccountSearchController.js
@@ -1,5 +1,25 @@
+/**
+ * @File Name          : AccountSearchController.js
+ * @Description        : 
+ * @Author             : KyleGibson
+ * @Group              : 
+ * @Last Modified By   : KyleGibson
+ * @Last Modified On   : 2019/07/08, 11:30:35
+ * @Modification Log   : 
+ *==============================================================================
+ * Ver         Date                     Author      		      Modification
+ *==============================================================================
+ * 1.0    2019/07/08, 11:30:35   KyleGibson     Initial Version
+**/
 ({
-    myAction : function(component, event, helper) {
-
+    onInit: function( component, event, helper ) {
+        // proactively search on component initialization
+        var searchTerm = component.get( "v.searchTerm" );
+        helper.handleSearch( component, searchTerm );
+    },
+    onSearchTermChange: function( component, event, helper ) {
+        // search anytime the term changes in the input field
+        var searchTerm = component.get( "v.searchTerm" );
+        helper.handleSearch( component, searchTerm );
     }
 })
