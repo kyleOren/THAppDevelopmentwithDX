906a9d1e47d64416a41e8bab950c81b2b2f5204d
Kyle Gibson <kyle.gibson@smarterbusiness.co.uk>
2019-07-08 11:31:30 +0200

    updated project with requirements from trailhead - build the geolocation app using salesforce cli

--- geolocation/force-app/main/default/aura/AccountSearch/AccountSearchHelper.js
+++ geolocation/force-app/main/default/aura/AccountSearch/AccountSearchHelper.js
@@ -1,5 +1,31 @@
+/**
+ * @File Name          : AccountSearchHelper.js
+ * @Description        : 
+ * @Author             : KyleGibson
+ * @Group              : 
+ * @Last Modified By   : KyleGibson
+ * @Last Modified On   : 2019/07/08, 11:31:21
+ * @Modification Log   : 
+ *==============================================================================
+ * Ver         Date                     Author      		      Modification
+ *==============================================================================
+ * 1.0    2019/07/08, 11:31:21   KyleGibson     Initial Version
+**/
 ({
-    helperMethod : function() {
-
+    // code in the helper is reusable by both
+    // the controller.js and helper.js files
+    handleSearch: function( component, searchTerm ) {
+        var action = component.get( "c.searchAccounts" );
+        action.setParams({
+            searchTerm: searchTerm
+        });
+        action.setCallback( this, function( response ) {
+            var event = $A.get( "e.c:AccountsLoaded" );
+            event.setParams({
+                "accounts": response.getReturnValue()
+            });
+            event.fire();
+        });
+        $A.enqueueAction( action );
     }
 })
